f52d98fad9d9918ecb5e62e915fe8111a1efc038
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-07-09 17:43:45 -0300

    Implementing logic two

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -5,17 +5,15 @@ import Button from "../components/Button";
 import Display from "../components/Display";
 
 const initialState = {
-    displayValue: '0',
+    displayValue: "0",
     clearDisplay: false,
     operation: null,
-    values: [0,0],
-    current: 0
-}
+    values: [0, 0],
+    current: 0,
+};
 
 export default class Calculator extends Component {
-
-    state = {... initialState}
-
+    state = { ...initialState };
 
     constructor(props) {
         super(props);
@@ -26,30 +24,56 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        this.setState({... initialState})
+        this.setState({ ...initialState });
     }
 
     setOperation(operation) {
-        console.log(operation);
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+        } else {
+            const equals = operation === "=";
+            const currentOperation = this.state.operation;
+
+            const values = [...this.state.values];
+
+            try {
+                values[0] = eval(
+                    `${values[0]} ${currentOperation} ${values[1]}`
+                );
+            } catch (e) {
+                values[0] = this.state.values[0];
+            }
+
+            values[1] = 0;
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values,
+            });
+        }
     }
 
     addDigit(n) {
-        if ( n === '.' && this.state.displayValue.includes('.')) {
-            return 
+        if (n === "." && this.state.displayValue.includes(".")) {
+            return;
         }
 
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay 
-        const currentValue = clearDisplay ? '' : this.state.displayValue
-        const displayValue = currentValue + n
-        this.setState({displayValue, clearDisplay: false})
+        const clearDisplay =
+            this.state.displayValue === "0" || this.state.clearDisplay;
+        const currentValue = clearDisplay ? "" : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({ displayValue, clearDisplay: false });
 
-        if (n !== '.') {
-            const i = this.state.current
-            const newValue = parseFloat(displayValue)
-            const values = [...this.state.values]
-            values[i] = newValue
-            this.setState({ values })
-            console.log(values)
+        if (n !== ".") {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values });
+            console.log(values);
         }
     }
 
